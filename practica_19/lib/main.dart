4d0311ea7da337c5f0f9e295fbc3c462359567a0
C3j45 <walkerfluctus@gmail.com>
2023-11-12 03:59:16 -0700

    Proyect

---
+++ practica_19/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:practica_19/src/listview.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Practica 19',
+      home: ListViewPage(),
+    );
+  }
+}
